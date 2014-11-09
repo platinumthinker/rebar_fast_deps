0d79d0158b076796a563d658e90af6355eea6fea
Andrey Teplyashin <platinumthinker@gmail.com>
2014-11-09 12:23:54 +0600

    Fix include headers from rebar

--- src/rebar_fast_deps.erl
+++ src/rebar_fast_deps.erl
@@ -4,7 +4,7 @@
          'fast-update-deps'/2
         ]).
 
--include_lib("rebar/include/rebar.hrl").
+-include_lib("rebar.hrl").
 
 'fast-update-deps'(Config, _AppFile) ->
     DepsDir = rebar_config:get(Config, deps_dir, "deps"),
